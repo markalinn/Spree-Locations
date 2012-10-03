8048295df1c2a1aa29b65c0a7431d4db38f76312
Mark Linn <marklinn@xwcsolutions.com>
2012-10-03 12:16:32 -0700

    update to admin edit functionality to match newer spree methodology

--- app/controllers/spree/admin/locations_controller.rb
+++ app/controllers/spree/admin/locations_controller.rb
@@ -2,6 +2,12 @@ module Spree
   module Admin
     class LocationsController < ResourceController
       
+      protected
+      
+      def location_after_save
+        admin_locations_url
+      end
+      
     end
   end
 end
